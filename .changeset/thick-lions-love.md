3c5ce3b5cf15d303b8c19a30d68fe13527556c42
Benjamin Simmers <bej1380@gmail.com>
2024-10-20 17:43:28 +1000

    chore: export window components correctly

---
+++ .changeset/thick-lions-love.md
@@ -0,0 +1,5 @@
+---
+'windows-98-ui': patch
+---
+
+export window components correctly
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-export { Button } from "./components/button";
-export { Dropdown } from "./components/dropdown";
-export { TextBox } from "./components/textbox";
-export { Slider } from "./components/slider"
-export { TitleBar } from "./components/titleBar";
+export { Button } from './components/button';
+export { Dropdown } from './components/dropdown';
+export { TextBox } from './components/textbox';
+export { Slider } from './components/slider';
+export { TitleBar } from './components/titleBar';
+export { Window } from './components/window/windowContainer';
+export { WindowBody } from './components/window/windowBody';
